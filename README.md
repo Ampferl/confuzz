484e7a69f52c9d1cbb5f03f44cce5b91d46f8cda
Jonas Ampferl <ampferl.jonas@gmail.com>
2025-11-25 18:10:02 +0100

    fix: Add method to intercept producer responses with Burp Suite

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ConFuzz
 ConFuzz is a novel LLM-driven Fuzzer designed for security testing the unsafe consumption APIs.
+
+## Intercepting Responses with Burp Suite
+To intercept responses with Burp Suite, a few things need to be adjusted:
+1. Burp must be configured in the proxy settings so that it listens on all interfaces.
+2. Burp must activate the option to intercept server responses in the proxy settings and in the rules, activate the condition that responses should be intercepted if the request has already been activated.
--- test-environment/consumer/utils.py
+++ test-environment/consumer/utils.py
@@ -10,11 +10,16 @@ logger = logging.getLogger("consumer")
 
 
 async def fetch_data_from_producer(url):
-    async with httpx.AsyncClient(proxies=os.getenv("HTTP_PROXY"), timeout=None) as client:
+    mounts = {}
+    if PROXY_URL:
+        mounts = {"http://": httpx.AsyncHTTPTransport(proxy=PROXY_URL)}
+
+    async with httpx.AsyncClient(mounts=mounts, verify=False, timeout=None) as client:
         try:
             resp = await client.get(f"{PRODUCER_URL}{url}")
             return resp
         except Exception as e:
+            logger.error(f"PROXY_URL: {PROXY_URL}")
             logger.error(f"Error fetching data from producer: {e}")
             return  None
 
